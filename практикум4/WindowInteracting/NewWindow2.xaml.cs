a46ab04e3ab67962649f8def4fe00a7d19748fb0
NairGor <56095728+NairGor@users.noreply.github.com>
2022-12-08 00:17:53 +0300

    Add files via upload

---
+++ практикум4/WindowInteracting/NewWindow2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для NewWindow2.xaml
+    /// </summary>
+    public partial class NewWindow2 : Window
+    {
+        public NewWindow2()
+        {
+            void UpdateWindow(string message);
+        }
+    }
+}
